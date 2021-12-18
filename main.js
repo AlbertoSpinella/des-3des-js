1ccc18c7466766e4d90af54a8c05f5631ecd35ef
Alberto <alberto.spinella@protonmail.com>
2021-12-18 20:55:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+import {
+    hexToBin,
+    padKey,
+    PC1,
+    shiftAfterPC1
+} from "./libs/utils.js";
+
+const plaintext = "0123456789ABCDEF";
+const key = "133457799BBCDFF1";
+
+const splittedKey = {};
+
+const keySchedulation = (key) => {
+    const paddedKey = padKey(key);
+    const binaryKey = hexToBin(paddedKey);
+    const permuted1Key = PC1(binaryKey);
+    const cArray = [];
+    const dArray = [];
+    cArray.push(permuted1Key.substring(0, permuted1Key.length/2));
+    dArray.push(permuted1Key.substring(permuted1Key.length/2, permuted1Key.length));
+    shiftAfterPC1(cArray, dArray);
+};
+
+keySchedulation(key);
