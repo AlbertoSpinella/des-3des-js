09d317a152eb54ac2998043aec7b639f3770d2ff
Alberto <alberto.spinella@protonmail.com>
2021-12-26 12:03:04 +0100

    add DES decryption

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 import { keySchedulation } from "./libs/keySchedulation.js";
 import { desEncryption } from "./libs/desEncryption.js";
 
-export const main = (plaintext, key) => {
+export const desEncrypt = (plaintext, key) => {
     console.log({ plaintext, key });
     const permutedKeys = keySchedulation(key);
 
     const ciphertext = desEncryption(plaintext, permutedKeys);
+
     return ciphertext;
 };
 
-console.log({ ciphertext: main("0123456789ABCDEF", "133457799BBCDFF1") });
+export const desDecrypt = (ciphertext, key) => {
+    console.log({ ciphertext, key });
+    const permutedKeys = keySchedulation(key);
+
+    const reversedKeys = permutedKeys.reverse();
+    const plaintext = desEncryption(ciphertext, reversedKeys);
+
+    return plaintext;
+};
+
+console.log("DES encryption...");
+const encrypted = desEncrypt("0123456789ABCDEF", "133457799BBCDFF1");
+console.log({ ciphertext: encrypted });
+
+console.log("\nDES decryption...");
+const decrypted = desDecrypt(encrypted, "133457799BBCDFF1");
+console.log({ plaintext: decrypted });
