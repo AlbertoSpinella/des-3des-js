6b615ca1c7c44148a6e7cd4b03598ce9d4606c36
Alberto <alberto.spinella@protonmail.com>
2021-12-27 12:41:34 +0100

    add ofb random test

--- __random__test__/random.test.js
+++ __random__test__/random.test.js
@@ -3,10 +3,12 @@ import { keySchedulation } from "../libs/keySchedulation.js";
 import { desEcb } from "../libs/desEcb.js";
 import { desCbcEncryption } from "../libs/desCbc.js";
 import { desCfbEncryption } from "../libs/desCfb.js";
+import { desOfbEncryption } from "../libs/desOfb.js";
 import {
     standardDesEcb,
     standardDesCbc,
-    standardDesCfb
+    standardDesCfb,
+    standardDesOfb
 } from "../libs/test/standard-des.js";
 
 const RANDOM_TEST_AMOUNT = 128;
@@ -42,6 +44,18 @@ for (let i=0; i<RANDOM_TEST_AMOUNT; i++) {
         const permutedKeys = keySchedulation(hexKey);
         const ciphertextToTest = desCfbEncryption(hexPlaintext, permutedKeys, hexIv, "encryption");
         const ciphertextResult = standardDesCfb(hexKey, hexPlaintext, hexIv);
+        expect(ciphertextToTest).toBe(ciphertextResult);
+    });
+}
+
+for (let i=0; i<RANDOM_TEST_AMOUNT; i++) {
+    const hexKey = randomHexString(16);
+    const hexPlaintext = randomHexString(16);
+    const hexIv = randomHexString(16);
+    test(`DES OFB - Random key and plaintext test - ${i+1}/${RANDOM_TEST_AMOUNT}`, ()  => {
+        const permutedKeys = keySchedulation(hexKey);
+        const ciphertextToTest = desOfbEncryption(hexPlaintext, permutedKeys, hexIv);
+        const ciphertextResult = standardDesOfb(hexKey, hexPlaintext, hexIv);
         expect(ciphertextToTest).toBe(ciphertextResult);
     });
 }
--- libs/test/standard-des.js
+++ libs/test/standard-des.js
@@ -43,3 +43,18 @@ export const standardDesCfb = (hexKey, hexPlaintext, hexIv) => {
         process.exit(1);
     }
 };
+
+export const standardDesOfb = (hexKey, hexPlaintext, hexIv) => {
+    const key = Buffer.from(hexKey, "hex");
+    const plaintext = Buffer.from(hexPlaintext, "hex");
+    const iv = Buffer.from(hexIv, "hex");
+    
+    try {
+        const cipher = crypto.createCipheriv("des-ofb", key, iv);
+        const ciphertext = cipher.update(plaintext, "utf-8", "hex").toUpperCase();
+        return ciphertext;
+    } catch (error) {
+        console.log("ERR", error);
+        process.exit(1);
+    }
+};
