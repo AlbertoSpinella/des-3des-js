1fae39b49a40d95f5017afdbb72160a053bff116
AlbertoSpinella <alberto.spinella@protonmail.com>
2022-12-26 11:21:11 +0100

    add 3DES OFB (E & D)

---
+++ 3desOfb.js
@@ -0,0 +1,77 @@
+import { keySchedulation } from "./libs/keySchedulation.js";
+import {
+    desEcbEncryptionSingleBlock
+} from "./libs/desEcbUtils.js";
+import { binToHex, hexToBin, xor } from "./libs/utils.js";
+
+export const tripleDesOfbEncrypt = (plaintext, key, iv) => {
+    if (key.length != 48) throw new Error("ERR_KEY_LENGTH_NOT_48");
+    console.log({ plaintext, key, iv });
+
+    const key1 = key.substring(0, 16);
+    const key2 = key.substring(16, 32);
+    const key3 = key.substring(32, 48);
+    const permutedKeys1 = keySchedulation(key1);
+    const permutedKeys2 = keySchedulation(key2);
+    const reversedKeys2 = [...permutedKeys2].reverse();
+    const permutedKeys3 = keySchedulation(key3);
+
+    const plaintextBlocks = [];
+    const ciphertextBlocks = [];
+    for (let i=0; i < plaintext.length; i+=16)
+        plaintextBlocks.push(plaintext.substring(i, i + 16));
+    for (let plaintextBlock of plaintextBlocks) {
+        if (plaintextBlock.length != 16) plaintextBlock = padTo16Bytes(plaintextBlock);
+        const firstRound = desEcbEncryptionSingleBlock(iv, permutedKeys1);
+        const secondRound = desEcbEncryptionSingleBlock(firstRound, reversedKeys2);
+        const thirdRound = desEcbEncryptionSingleBlock(secondRound, permutedKeys3);
+        const thirdRoundBin = hexToBin(thirdRound);
+        const plaintextBlockBin = hexToBin(plaintextBlock);
+        const ciphertextBlockHex = xor(thirdRoundBin, plaintextBlockBin);
+        const ciphertextBlock = binToHex(ciphertextBlockHex);
+        iv = thirdRound;
+        ciphertextBlocks.push(ciphertextBlock);
+    }
+    const ciphertext = ciphertextBlocks.join("");
+    return ciphertext;
+};
+
+export const tripleDesOfbDecrypt = (ciphertext, key, iv) => {
+    if (key.length != 48) throw new Error("ERR_KEY_LENGTH_NOT_48");
+    console.log({ ciphertext, key, iv });
+
+    const key1 = key.substring(0, 16);
+    const key2 = key.substring(16, 32);
+    const key3 = key.substring(32, 48);
+    const permutedKeys1 = keySchedulation(key1);
+    const permutedKeys2 = keySchedulation(key2);
+    const reversedKeys2 = [...permutedKeys2].reverse();
+    const permutedKeys3 = keySchedulation(key3);
+
+    const ciphertextBlocks = [];
+    const plaintextBlocks = [];
+    for (let i=0; i < ciphertext.length; i+=16)
+        ciphertextBlocks.push(ciphertext.substring(i, i + 16));
+    for (const ciphertextBlock of ciphertextBlocks) {
+        if (ciphertextBlock.length != 16) throw new Error("ERR_INVALID_CIPHERTEXT_LENGTH");
+        const firstRound = desEcbEncryptionSingleBlock(iv, permutedKeys1);
+        const secondRound = desEcbEncryptionSingleBlock(firstRound, reversedKeys2);
+        const thirdRound = desEcbEncryptionSingleBlock(secondRound, permutedKeys3);
+        const thirdRoundBin = hexToBin(thirdRound);
+        const ciphertextBlockBin = hexToBin(ciphertextBlock);
+        const plaintextBlockHex = xor(thirdRoundBin, ciphertextBlockBin);
+        const plaintextBlock = binToHex(plaintextBlockHex);
+        plaintextBlocks.push(plaintextBlock);
+        iv = thirdRound;
+    }
+    const plaintext = plaintextBlocks.join("");
+    return plaintext;
+};
+
+console.log("\n3 DES OFB encryption...");
+const desOfbEncrypted = tripleDesOfbEncrypt("0123456789ABCDEF0A1B2C3D4E5F6A7B", "133457799BBCDFF1475433098D5C01704F82A025AA2EE8BB", "AABBCCDDAABBCCDD");
+console.log({ ciphertext: desOfbEncrypted });
+
+console.log("\nDES OFB decryption...");
+const desOfbDecrypted = tripleDesOfbDecrypt(desOfbEncrypted, "133457799BBCDFF1475433098D5C01704F82A025AA2EE8BB", "AABBCCDDAABBCCDD");
+console.log({ plaintext: desOfbDecrypted });
